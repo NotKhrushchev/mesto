00436878e7da78113237018ef204b59c978a9a6d
Nikita Ipatov <Nikitafilemine1@gmail.com>
2023-03-07 11:14:51 +0300

    feat: добавить функцию закрытия и изменения данных профиля по нажатию на кнопку "Сохранить"

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+let profileEditBtn = document.querySelector('.profile__edit-btn');
+let profileName = document.querySelector('.profile__name');
+let profileInterest = document.querySelector('.profile__interest');
+let popup = document.querySelector('.popup');
+let popupName = document.querySelector('.popup__name');
+let popupInterest = document.querySelector('.popup__interest');
+let popupCloseBtn = document.querySelector('.popup__close-btn');
+let formElement = document.querySelector('.popup__form');
+
+console.log(formElement)
+
+function openPopup () {
+    popup.style.visibility = 'visible'
+    popupName.value = profileName.textContent;
+    popupInterest.value = profileInterest.textContent
+};
+
+function closePopup () {
+    popup.style.visibility = 'hidden'
+};
+
+profileEditBtn.addEventListener('click', openPopup);
+
+popupCloseBtn.addEventListener('click', closePopup);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    profileName.textContent = popupName.value
+    profileInterest.textContent = popupInterest.value
+    closePopup()
+};
+
+formElement.addEventListener('submit', handleFormSubmit);
