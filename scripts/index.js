7414e43ce5cce4171fd3b356ccf5380590c6589c
Nikita Ipatov <Nikitafilemine1@gmail.com>
2023-03-07 22:40:03 +0300

    fix: исправить ошибки в скрипте, файле html и read.me

--- scripts/index.js
+++ scripts/index.js
@@ -2,32 +2,34 @@ let profileEditBtn = document.querySelector('.profile__edit-btn');
 let profileName = document.querySelector('.profile__name');
 let profileInterest = document.querySelector('.profile__interest');
 let popup = document.querySelector('.popup');
-let popupName = document.querySelector('.popup__name');
-let popupInterest = document.querySelector('.popup__interest');
+let popupName = document.querySelector('.popup__input_name');
+let popupInterest = document.querySelector('.popup__input_interest');
 let popupCloseBtn = document.querySelector('.popup__close-btn');
 let formElement = document.querySelector('.popup__form');
 
-console.log(formElement)
-
 function openPopup () {
-    popup.style.display = 'flex'
-    popupName.value = profileName.textContent;
-    popupInterest.value = profileInterest.textContent
+    popup.classList.add('popup_opened');
+    putProfileValueIntoPopupForm();
 };
 
 function closePopup () {
-    popup.style.display = 'none'
+    popup.classList.remove('popup_opened');
+};
+
+function putProfileValueIntoPopupForm () {
+    popupName.value = profileName.textContent;
+    popupInterest.value = profileInterest.textContent;
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    profileName.textContent = popupName.value;
+    profileInterest.textContent = popupInterest.value;
+    closePopup();
 };
 
 profileEditBtn.addEventListener('click', openPopup);
 
 popupCloseBtn.addEventListener('click', closePopup);
-
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-    profileName.textContent = popupName.value
-    profileInterest.textContent = popupInterest.value
-    closePopup()
-};
 
 formElement.addEventListener('submit', handleFormSubmit);
