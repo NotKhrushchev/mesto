d0f1d2f9f6a7e2d9c5051efdbb7c03b39b79ecff
Nikita Ipatov <Nikitafilemine1@gmail.com>
2023-03-14 21:49:34 +0300

    feat: добавить карточки из массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,68 @@
-let profileEditBtn = document.querySelector('.profile__edit-btn');
-let profileName = document.querySelector('.profile__name');
-let profileInterest = document.querySelector('.profile__interest');
-let popup = document.querySelector('.popup');
-let popupName = document.querySelector('.popup__input_type_name');
-let popupInterest = document.querySelector('.popup__input_type_interest');
-let popupCloseBtn = document.querySelector('.popup__close-btn');
-let formElement = document.querySelector('.form');
+const profileEditBtn = document.querySelector('.profile__edit-btn');
+const profileName = document.querySelector('.profile__name');
+const profileInterest = document.querySelector('.profile__interest');
+const popup = document.querySelector('.popup');
+const popupName = document.querySelector('.popup__input_type_name');
+const popupInterest = document.querySelector('.popup__input_type_interest');
+const popupCloseBtn = document.querySelector('.popup__close-btn');
+const formElement = document.querySelector('.form');
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
 
-function openPopup () {
+const createCard = (data) => {
+    const newCard = document.querySelector('.card-template').content.querySelector('.card').cloneNode(true);
+    const cardImg = newCard.querySelector('.card__img');
+    const cardDesc = newCard.querySelector('.card__desc');
+    cardImg.setAttribute('src', data.link);
+    cardDesc.textContent = data.name;
+    return newCard;
+};
+
+initialCards.forEach((data) => {
+    const card = createCard(data);
+    const cardArrea = document.querySelector('.cards');
+    cardArrea.append(card);
+});
+
+const openPopup = () => {
     popup.classList.add('popup_opened');
 };
 
-function closePopup () {
+const closePopup = () => {
     popup.classList.remove('popup_opened');
 };
 
-function editForm () {
+const editForm = () => {
     openPopup();
     popupName.value = profileName.textContent;
     popupInterest.value = profileInterest.textContent;
 };
 
-function handleFormSubmit (evt) {
+const handleFormSubmit = (evt) => {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileInterest.textContent = popupInterest.value;
